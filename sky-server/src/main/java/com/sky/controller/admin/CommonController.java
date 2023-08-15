ac6ba1110c1ae2399ac06434c0a89da5fdc9d7a7
Baymine <978176728@qq.com>
2023-08-15 22:56:44 +0800

    dish: 图片上传到OSS

---
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -0,0 +1,53 @@
+package com.sky.controller.admin;
+
+import com.sky.result.Result;
+import com.sky.utils.AliOssUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/admin/common")
+@Api(tags = "通用接口")
+@Slf4j
+public class CommonController {
+
+    @Autowired
+    private AliOssUtil aliOssUtil;
+
+    /**
+     * 文件上传
+     * @param file
+     * @return
+     */
+    @PostMapping("/upload")
+    @ApiOperation(value = "文件上传")
+    public Result<String> upload(MultipartFile file) {
+      log.info("上传文件: {}", file);
+
+      try {
+          String originalFilename = file.getOriginalFilename();
+          assert originalFilename != null;
+          String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+          // 使用uuid对上传的文件进行重命名，这是为了防止重名导致的文件覆盖
+          String objectName = UUID.randomUUID() + extension;
+
+          String filePath = aliOssUtil.upload(file.getBytes(), objectName);
+
+          return Result.success(filePath);
+
+      } catch (Exception e) {
+          log.error("上传文件失败: {}", e.getMessage());
+      }
+
+
+      return null;
+    }
+}
